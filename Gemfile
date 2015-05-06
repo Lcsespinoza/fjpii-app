1aaf64ae4911f50b3a6a0577a48a2785be7a9db9
lucas <lcsespinoza@gmail.com>
2015-05-05 23:10:51 -0300

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'rails', '4.2.0'
 
 gem 'bootstrap-sass', '2.3.2.0'
 
-gem 'bcrypt-ruby', '3.1.2'
+gem 'bcrypt-ruby'
 
 
 # Use sqlite3 as the database for Active Record
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
   end
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)    
+    if @user.save
+      flash[:success] = "Exbecado creado con éxito!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:rut, :num_alumno, :perfil, :sexo, :nombre, :paterno, :materno, :fecha_nacimiento, :estado_civil, 
+      							   :hijos, :celular, :direccion, :comuna, :ciudad, :region, :postal, :pais, :telefono, :direccion_lab, 
+      							   :comuna_lab, :email, :password, :password_confirmation,  :ciudad_lab, :region_lab, :postal_lab, 
+      							   :pais_lab, :telefono_lab, :universidad, :carrera, :especialidad, :ano_ing_u, :ano_egr_u, :ano_ing_fjp, 
+      							   :religion, :adhesion_rel, :actividad, :directorio)
+    end
 end
